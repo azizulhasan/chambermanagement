d5d8d1c242f475b9fa85bf26acba700e78838d19
Azizul Hasan <azizulhasan.webappick@gmail.com>
2023-02-05 20:36:15 +0600

    CM-10: slide issue fixed.

--- client/src/components/front/home/sections/Professionals.js
+++ client/src/components/front/home/sections/Professionals.js
@@ -1,7 +1,10 @@
 // https://www.npmjs.com/package/react-responsive-carousel
 
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
+import { decideTotalSlides, fillArray } from "../../../../utilities/utilities";
+
+
 
 const doctorInformations = [
     { specialist: "PHYCHOLOGIST", name: "Dr. Mr. Madadi Hasan", degree: "", img: './assets/front/images/corousel/1.jpeg' },
@@ -13,6 +16,7 @@ const doctorInformations = [
 ]
 
 
+let currentIndex = 0;
 
 const Professionals = () => {
     const [perSlideWidth, setPerSlideWith] = useState(100)
@@ -20,28 +24,11 @@ const Professionals = () => {
     const [totalSlides, setTotalSlides] = useState([])
     const [screenWidth, setScreenWidth] = useState(window.innerWidth)
 
-    function decideTotalSlides() {
-        let srWidth = window.screen.width
-        let slideWidth = 100;
-        let slideInARow = 1;
-        if (srWidth < 575) {
-            slideWidth = parseInt(srWidth / 1)
-            slideInARow = 1;
-        } else if (srWidth >= 575 && srWidth < 768) {
-            slideWidth = Math.ceil(srWidth / 3)
-            slideInARow = 3;
-        } else if (srWidth >= 768 && srWidth < 992) {
-            slideWidth = Math.ceil(srWidth / 4)
-            slideInARow = 4;
-        } else {
-            slideWidth = Math.ceil(srWidth / 5)
-            slideInARow = 5;
-        }
-        setPerSlideWith(slideWidth + "px")
-        setItemsInSingleSlide(fillArray(slideInARow))
-    }
+
     useEffect(() => {
-        decideTotalSlides()
+        let data = decideTotalSlides()
+        setPerSlideWith(data.perSlideWidth)
+        setItemsInSingleSlide(data.itemsInSingleSlide)
     }, [])
 
     useEffect(() => {
@@ -56,59 +43,63 @@ const Professionals = () => {
 
     }, [itemsInSingleSlide])
 
-    function fillArray(length) {
-        let data = []
-        for (let i = 0; i < length; i++) {
-            data.push(i)
-        }
-        return data;
-    }
+
     useEffect(() => {
         console.log({ width: screenWidth, itemsInSingleSlide: itemsInSingleSlide, perSlideWidth: perSlideWidth, totalSlides: totalSlides });
     }, [totalSlides])
 
     return (
-        <Carousel
-            autoPlay={false}
-            showArrows={true}
-            emulateTouch={true}
-            infiniteLoop={true}
-            stopOnHover={true}
-            showThumbs={false}
-            className="presentation-mode professional mt-2 px-5"
-        >
-            {
-                totalSlides.length && totalSlides.map(i => {
-                    return <div key={i} className="flex">
-                        {
-                            itemsInSingleSlide.map(index => {
-                                return (< div key={index} style={{ width: perSlideWidth }} className={["flex-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl pr-2",].join(' ')} >
-                                    <img
-                                        className="h-60 object-cover rounded-xl"
-                                        src={doctorInformations[index].img}
-                                        alt=""
-                                    />
-                                    <div className="p-2">
-                                        <h2 className="font-bold text-m text-themeColor">{doctorInformations[index].specialist}</h2>
-                                        <h2 className="font-bold text-lg mb-2">{doctorInformations[index].name}</h2>
-
-                                        <div className="m-2">
-                                            <a
-                                                role="button"
-                                                href="/"
-                                                className="text-white bg-themeColor px-3 py-1 flex flex-nowrap rounded-md hover:bg-themeColor"
-                                            >
-                                                Book An Appointment
-                                            </a>
+        <div className=" my-10">
+            <h1 className="text-2xl font-bold tracking-tight text-gray-900 pl-5">Health Professionals</h1>
+            <Carousel
+                autoPlay={false}
+                showArrows={true}
+                emulateTouch={true}
+                infiniteLoop={true}
+                stopOnHover={true}
+                showThumbs={false}
+                className="presentation-mode professional pt-3 px-5"
+            >
+                {
+                    totalSlides.length && totalSlides.map(i => {
+                        return <div key={i} className="flex">
+                            {
+                                itemsInSingleSlide.map(index => {
+                                    if (i !== 0) {
+                                        currentIndex = currentIndex + 1;
+                                        if (currentIndex >= doctorInformations.length) {
+                                            currentIndex = 0;
+                                        }
+                                    } else {
+                                        currentIndex = index
+                                    }
+                                    return (< div key={currentIndex} style={{ width: perSlideWidth }} className={["flex-2 bg-white rounded-xl transform transition-all hover:-translate-y-2 duration-300 shadow-lg hover:shadow-2xl pr-2",].join(' ')} >
+                                        <img
+                                            className="h-60 object-cover rounded-xl"
+                                            src={doctorInformations[currentIndex].img}
+                                            alt=""
+                                        />
+                                        <div className="p-2">
+                                            <h2 className="font-bold text-m text-themeColor">{doctorInformations[currentIndex].specialist}</h2>
+                                            <h2 className="font-bold text-lg mb-2">{doctorInformations[currentIndex].name}</h2>
+                                            <div className="m-2">
+                                                <a
+                                                    role="button"
+                                                    href="/"
+                                                    className="text-white bg-themeColor px-3 py-1 flex flex-nowrap rounded-md hover:bg-themeColor"
+                                                >
+                                                    Book An Appointment
+                                                </a>
+                                            </div>
                                         </div>
-                                    </div>
-                                </div>)
-                            })
-                        }
-                    </div>
-                })
-            }
-        </Carousel>
+                                    </div>)
+                                })
+                            }
+                        </div>
+                    })
+                }
+            </Carousel>
+        </div>
     );
 };
 
